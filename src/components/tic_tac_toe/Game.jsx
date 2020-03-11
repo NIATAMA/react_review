9cdda86a98983cccbba47e541fbf03c44234b61b
NIATAMA <943070101@qq.com>
2020-03-11 17:46:41 +0800

    井字棋-基本游戏功能

--- src/components/tic_tac_toe/Game.jsx
+++ src/components/tic_tac_toe/Game.jsx
@@ -2,23 +2,70 @@ import React from 'react'
 import './tictac.css'
 import './tictac.js'
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
+// 判断是否出现赢家
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
+    return null;
+}
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        }
+    }
+
+    updateBoardState = (i) => {
+        // 数据不可变，故直接复制个新的数组
+        const squares = this.state.squares.slice()
+        // 赢家已出现/某格已被填充
+        if (calculateWinner(squares) || squares[i]) return
+        // 游戏继续
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState(state => ({ squares, xIsNext: !state.xIsNext }))
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return <Square value={this.state.squares[i]} onClick={() => this.updateBoardState(i)} />;
     }
 
     render() {
-        const status = 'Next player: X';
+        // 状态
+        const winner = calculateWinner(this.state.squares)
+        let status
+
+        if (winner) {
+            // 出现赢家
+            status = 'Winner is ' + winner
+        } else {
+            // 无赢家
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+        }
 
         return (
             <div>
